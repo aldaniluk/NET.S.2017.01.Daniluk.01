1eb64e36dd290bd44b8ed494177ae4cdfdecd633
adaniluk <allldaniluk@gmail.com>
2017-06-26 18:08:40 +0300

    Added 'Logic' namespace and class Sort

---
+++ Logic/Sort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class Sort
+    {
+        public static void QuickSort(int[] array)
+        {
+
+        }
+
+        public static void MergeSort(int[] array)
+        {
+
+        }
+    }
+}
