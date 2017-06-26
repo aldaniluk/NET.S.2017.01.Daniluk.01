18ad3a4bb4f304743beb3c1b0c1d12a169a3f635
adaniluk <allldaniluk@gmail.com>
2017-06-26 18:14:35 +0300

    Added MergeSort /*from interview:)*/

--- Logic/Sort.cs
+++ Logic/Sort.cs
@@ -15,7 +15,46 @@ namespace Logic
 
         public static void MergeSort(int[] array)
         {
-
+            Divide(array, 0, array.Length - 1);
+        }
+        private static void Divide(int[] array, int left, int right)
+        {
+            if (right.CompareTo(left) == 0) return;
+            int middle = left + (right - left) / 2;
+            Divide(array, left, middle);
+            Divide(array, middle + 1, right);
+            Merge(array, left, middle, right);
+        }
+        private static void Merge(int[] array, int left, int middle, int right)
+        {
+            int positionL = left;
+            int positionR = middle + 1;
+            int[] result = new int[right - left + 1];
+            int positionResult = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (positionL > middle)
+                {
+                    result[positionResult++] = array[positionR++];
+                }
+                else if (positionR > right)
+                {
+                    result[positionResult++] = array[positionL++];
+                }
+                else if (array[positionL].CompareTo(array[positionR]) < 0)
+                {
+                    result[positionResult++] = array[positionL++];
+                }
+                else
+                {
+                    result[positionResult++] = array[positionR++];
+                }
+            }
+            for (int i = 0; i < result.Length; i++)
+            {
+                array[left + i] = result[i];
+            }
+            return;
         }
     }
 }
